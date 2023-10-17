b49bed92524d44a6f7fae59f3bb4fcf8a0d30bf5
Sergey-2023ch <639j1mo64aaz@mail.ru>
2023-10-17 09:54:16 +0300

    second commit

--- src/Ozon.java
+++ src/Ozon.java
@@ -2,12 +2,24 @@ import java.util.Scanner;
 
 public class Ozon {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
+
+        Scanner s = new Scanner(System.in);
+
         System.out.println("Введите наимненование товара: ");
-        String name= s.nextLine();
+        String name = s.nextLine();
+
         System.out.println("Количество: ");
-        int num=s.nextInt();
+        int num = s.nextInt();
+
         System.out.println("Телефон пользователя: ");
-        int fone=s.nextInt();
+        int phone = s.nextInt();
+        s.nextLine();
+
+        System.out.println("Адрес доставки : ");
+        String address = s.nextLine();
+
+        System.out.printf("Ваш заказ принят. Товар %s в количестве %d прибудет к вам в ближайшее время.\n Товар доставим по адресу %s . Ваш контактный номер %d.\n", name, num, address, phone);
+        s.close();
     }
+
 }
